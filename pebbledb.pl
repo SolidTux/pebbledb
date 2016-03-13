099a778f0179194753453905be79507f86f52bad
Daniel Hauck <mail@dhermit.de>
2016-03-13 22:31:38 +0100

    get next url

--- pebbledb.pl
+++ pebbledb.pl
@@ -3,12 +3,20 @@
 use strict;
 use warnings;
 use LWP::Simple;
+use JSON;
+use Data::Dumper;
 
 my $appurl = "https://api2.getpebble.com/v2/apps/collection/all/watchapps-and-companions";
 my $faceurl = "https://api2.getpebble.com/v2/apps/collection/all/watchfaces";
 
 sub GetApps{
-    my $url = "$appurl?limit=100&offset=100";
-    $data = get($url);
-    print "$data";
+    my ($url) = @_;
+    $url //= "$faceurl?limit=10&offset=10";
+    print "$url";
+    my $data = decode_json(get($url));
+    my $next = $data->{'links'}->{'nextPage'};
+    print "$next\n";
+    GetApps($next);
 }
+
+GetApps;
