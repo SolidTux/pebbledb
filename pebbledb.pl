992dd745ab93b8751d596f3a4048fef91fd295a0
Daniel Hauck <mail@dhermit.de>
2016-03-13 21:56:13 +0100

    json urls

--- pebbledb.pl
+++ pebbledb.pl
@@ -1,1 +1,14 @@
 #!/bin/perl
+
+use strict;
+use warnings;
+use LWP::Simple;
+
+my $appurl = "https://api2.getpebble.com/v2/apps/collection/all/watchapps-and-companions";
+my $faceurl = "https://api2.getpebble.com/v2/apps/collection/all/watchfaces";
+
+sub GetApps{
+    my $url = "$appurl?limit=100&offset=100";
+    $data = get($url);
+    print "$data";
+}
