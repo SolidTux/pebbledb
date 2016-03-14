ec3bd827f61f61fae3a160677e2333ed8b927895
Daniel Hauck <mail@dhermit.de>
2016-03-14 21:19:02 +0100

    added C++ code

--- pebbledb.pl
+++ pebbledb.pl
@@ -11,8 +11,8 @@ my $faceurl = "https://api2.getpebble.com/v2/apps/collection/all/watchfaces";
 
 sub GetApps{
     my ($url) = @_;
-    $url //= "$faceurl?limit=10&offset=10";
-    print "$url";
+    $url //= "$faceurl?limit=100";
+    print "$url\n";
     my $data = decode_json(get($url));
     my $next = $data->{'links'}->{'nextPage'};
     print "$next\n";
